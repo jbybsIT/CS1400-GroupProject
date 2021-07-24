95a13ec709c6624295a4cacee4a75b26930eb53c
irongut88 <87910873+irongut88@users.noreply.github.com>
2021-07-24 11:55:18 -0600

    Update Prompts.java
    Comments

--- CalorieCounter/Prompts.java
+++ CalorieCounter/Prompts.java
@@ -1,15 +1,22 @@
+
+//Import Scanner
 import java.util.Scanner;
 
+//Class Declaration
 public class Prompts
 {
+   //Converts user entries into variables
    static Scanner input = new Scanner(System.in);
    static int selection;
    
+   //Prompt Method
    public static void userInput()
    {
+      //Convert inputs
       UserInput userData = new UserInput();
       Scanner input = new Scanner(System.in);
       
+      //Prompt Series
       System.out.print("What is your name?: ");
       String name = input.nextLine();
       System.out.print("What is your gender? (M or F): ");
@@ -23,30 +30,30 @@ public class Prompts
       System.out.printf("What is your activity level?: %n");
       activitySelection();
       
-      // sends input to userInput class to be calculated
+      //Sends input to userInput class to be calculated
       userData.setGender(userGender);
       userData.setWeight(userWeight);
       userData.setHeight(userHeight);
       userData.setAge(userAge);
       userData.setActivity(selection);
       
-      // output 
-     // System.out.println(
       
    }
    
+   //Activity Selection
    private static void activitySelection()
    {
-      menuPrompt(); // call menuPrompt method
-      selection = input.nextInt(); // user prompt saved as selection variable
+      menuPrompt(); //Call menuPrompt method
+      selection = input.nextInt(); //User prompt saved as selection variable
    }
    
-   private static void menuPrompt() // creates menuPrompt method
+   //Creates menuPrompt method
+   private static void menuPrompt() 
 	{
-	   System.out.println("1 .. sedentary"); // output 1st line
-		System.out.println("2 .. moderate"); // output 2nd line
-		System.out.println("3 .. intense"); // output 3rd line
-		System.out.print("Your choice: "); // user input line
+	   System.out.println("1 .. sedentary"); //Output 1st line
+		System.out.println("2 .. moderate"); //Output 2nd line
+		System.out.println("3 .. intense"); //Output 3rd line
+		System.out.print("Your choice: "); //User input line
 	} 
 
 }
